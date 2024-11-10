d426906445f157d06d003a9cc24be5e7d4a0141d
Alejandro Bravo Isajar <alejandrobravoisajar1@gmail.com>
2024-11-09 19:37:29 -0500

    si

--- src/context/ReservesContext.jsx
+++ src/context/ReservesContext.jsx
@@ -23,71 +23,29 @@ const fireStore = getFirestore(fireDB);
 
 export const ReservesContext = createContext();
 export const ReservesProvider = ({ children }) => {
-  const [reserves, setReserves] = useState([]);
-  const [reserveData, setReserveData] = useState({
-    aula: null,
-    piso: null,
-    salon: null,
-    start: null,
-    end: null,
-    reason: "",
-    studentCode: "",
-    phone: "",
-  });
-
-  const createReserve = async (newReserve) => {
-    const isAvailable = await checkAvailability(
-      newReserve.room,
-      newReserve.start,
-      newReserve.end,
-      newReserve.date
-    );
-
-    if (isAvailable) {
-      const reserveData = {
-        start: newReserve.start,
-        end: newReserve.end,
-        room: newReserve.room,
-        reason: newReserve.reason,
-        studentCode: newReserve.studentCode,
-        phone: newReserve.phone,
-        date: newReserve.date,
-      };
-
-      await addDoc(collection(db, "reserves"), reserveData);
-      setReserves([...reserves, reserveData]);
-    } else {
-      alert("El salón ya está reservado en este horario. Elija otro horario.");
+  const createReserve = async ({
+    room,
+    studentCode,
+    phone,
+    start,
+    end,
+    reason,
+  }) => {
+    const reserveId = uuidv4();
+    try {
+      const docRef = doc(fireStore, "reserves", reserveId);
+      await setDoc(docRef, {
+        room,
+        studentCode,
+        phone,
+        start,
+        end,
+        reason,
+      });
+      console.log("Document written with ID: ", reserveId);
+    } catch (e) {
+      console.error("Error adding document: ", e);
     }
-  };
-
-  const checkAvailability = async (room, start, end, date) => {
-    const reservesRef = collection(db, "reserves");
-    const q = query(
-      reservesRef,
-      where("room", "==", room),
-      where("date", "==", date)
-    );
-
-    const querySnapshot = await getDocs(q);
-    let isAvailable = true;
-
-    querySnapshot.forEach((doc) => {
-      const reserve = doc.data();
-
-      const reserveStart = reserve.start;
-      const reserveEnd = reserve.end;
-
-      if (
-        (start >= reserveStart && start < reserveEnd) ||
-        (end > reserveStart && end <= reserveEnd) ||
-        (start <= reserveStart && end >= reserveEnd)
-      ) {
-        isAvailable = false;
-      }
-    });
-
-    return isAvailable;
   };
 
   const deleteReserve = async (reserveId, studentCode) => {
@@ -119,27 +77,9 @@ export const ReservesProvider = ({ children }) => {
     return reserves;
   };
 
-  useEffect(() => {
-    const interval = setInterval(async () => {
-      const now = new Date().getTime();
-      const reserves = await allReserves();
-      reserves.forEach((reserve) => {
-        const endTime = new Date(reserve.end).getTime();
-        if (now > endTime) deleteReserve(reserve.id);
-      });
-    }, 60000);
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <ReservesContext.Provider
-      value={{
-        createReserve,
-        deleteReserve,
-        allReserves,
-        reserveData,
-        setReserveData,
-      }}
+      value={{ createReserve, deleteReserve, allReserves }}
     >
       {children}
     </ReservesContext.Provider>
--- src/salonComponents/ModalRes.jsx
+++ src/salonComponents/ModalRes.jsx
@@ -1,92 +1,83 @@
 import React from "react";
 import { Button, Modal, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import { useForm } from "react-hook-form";
 import { useReserves } from "../context/ReservesContext";
 
-const ModalRes = ({ show, handleClose, room }) => {
+const ModalRes = ({ show, handleClose }) => {
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
+
   const { createReserve } = useReserves();
 
-  const availableHours = [
-    "09:00",
-    "10:00",
-    "11:00",
-    "12:00",
-    "13:00",
-    "14:00",
-    "15:00",
-    "16:00",
-    "17:00",
-    "18:00",
-  ];
-
   const onSubmit = handleSubmit((data) => {
-    createReserve({ ...data, room });
+    createReserve(data);
     handleClose();
   });
 
   return (
-    <Modal show={show} onHide={handleClose}>
-      <Modal.Header closeButton>
-        <Modal.Title>Reservar Salón</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            {...register("studentCode", { required: true })}
-            placeholder="Código Estudiante"
-          />
-          {errors.studentCode && <span>Este campo es requerido</span>}
-
-          <input
-            type="text"
-            {...register("phone", { required: true })}
-            placeholder="Teléfono"
-          />
-          {errors.phone && <span>Este campo es requerido</span>}
-
-          <label>Fecha</label>
-          <input type="date" {...register("date", { required: true })} />
-          {errors.date && <span>Este campo es requerido</span>}
-
-          <label>Hora de Inicio</label>
-          <select {...register("start", { required: true })}>
-            {availableHours.map((hour) => (
-              <option key={hour} value={hour}>
-                {hour}
-              </option>
-            ))}
-          </select>
-          {errors.start && <span>Este campo es requerido</span>}
-
-          <label>Hora de Fin</label>
-          <select {...register("end", { required: true })}>
-            {availableHours.map((hour) => (
-              <option key={hour} value={hour}>
-                {hour}
-              </option>
-            ))}
-          </select>
-          {errors.end && <span>Este campo es requerido</span>}
-
-          <input
-            type="text"
-            {...register("reason", { required: true })}
-            placeholder="Motivo"
-          />
-          {errors.reason && <span>Este campo es requerido</span>}
-
-          <Button variant="primary" type="submit">
-            Reservar
-          </Button>
-        </form>
-      </Modal.Body>
-    </Modal>
+    <>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header>Reservar</Modal.Header>
+        <Modal.Body>
+          <form onSubmit={onSubmit}>
+            <h4>Salon</h4>
+            <input
+              type="text"
+              {...register("room", { required: true })}
+              placeholder="room"
+            />
+            {errors.room && <span>This field is required</span>}
+            <h4>Codigo del estudiante</h4>
+            <input
+              type="text"
+              {...register("studentCode", { required: true })}
+              placeholder="studentCode"
+            />
+            {errors.studentCode && <span>This field is required</span>}
+            <h4>Telofono</h4>
+            <input
+              type="text"
+              {...register("phone", { required: true })}
+              placeholder="phone"
+            />
+            {errors.phone && <span>This field is required</span>}
+            <h4>Fecha de inicio</h4>
+            <input
+              type="text"
+              {...register("start", { required: true })}
+              placeholder="start"
+            />
+            {errors.start && <span>This field is required</span>}
+            <h4>Fecha de fin</h4>
+            <input
+              type="text"
+              {...register("end", { required: true })}
+              placeholder="end"
+            />
+            {errors.end && <span>This field is required</span>}
+            <h4>Razón</h4>
+            <input
+              type="text"
+              {...register("reason", { required: true })}
+              placeholder="reason"
+            />
+            {errors.reason && <span>This field is required</span>}
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Cancelar
+              </Button>
+              <Button type="submit" variant="primary">
+                Reservar
+              </Button>
+            </Modal.Footer>
+          </form>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 };
 
